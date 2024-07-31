ae7bde1ab672ee68a9f1162a8640c63699c91fc4
Mohamed Adel <111250674+Mohamedadel221@users.noreply.github.com>
2024-07-31 03:52:53 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,74 @@
+"use strict";
+// تحديد الروابط
+var homeLink = document.getElementById("homeLink");
+var aboutLink = document.getElementById("aboutLink");
+var servicesLink = document.getElementById("servicesLink");
+var articlesLink = document.getElementById("articlesLink");
+let navbar = document.getElementById("nav");
+
+
+// scroll navbar Add Background //
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 290) {
+    navbar.classList.add("top");
+  } else {
+    navbar.classList.remove("top");
+  }
+});
+// scroll navbar Add Background //
+
+// scroll buttun عنا //
+aboutLink.onclick = () => {
+  window.scrollTo({
+    left:0,
+    top:1450,
+    behavior:"smooth",
+  })
+};
+
+// scroll buttun عنا //
+
+// إضافة مُعالج الحدث لكل رابط
+homeLink.addEventListener("click", function () {
+  // إزالة الـ "class" النشطة من جميع الروابط
+  removeActiveClassFromAllLinks();
+  // إضافة الـ "class" النشطة للرابط الحالي
+  homeLink.classList.add("active");
+});
+
+aboutLink.addEventListener("click", function () {
+  removeActiveClassFromAllLinks();
+  aboutLink.classList.add("active");
+});
+
+
+servicesLink.addEventListener("click", function () {
+  removeActiveClassFromAllLinks();
+  servicesLink.classList.add("active");
+});
+
+articlesLink.addEventListener("click", function () {
+  removeActiveClassFromAllLinks();
+  articlesLink.classList.add("active");
+});
+
+// دالة لإزالة الـ "class" النشطة من جميع الروابط
+function removeActiveClassFromAllLinks() {
+  homeLink.classList.remove("active");
+  aboutLink.classList.remove("active");
+  servicesLink.classList.remove("active");
+  articlesLink.classList.remove("active");
+}
+function toggleSeeMore(contentId) {
+  var content = document.getElementById(contentId);
+  var moreText = content.querySelector("#more" + contentId.substr(7)); // Remove "content" from contentId to get the id of the more text
+  var btnText = content.querySelector("button");
+
+  if (moreText.style.display === "none" || moreText.style.display === "") {
+    moreText.style.display = "block";
+    btnText.textContent = "See less -";
+  } else {
+    moreText.style.display = "none";
+    btnText.textContent = "See more +";
+  }
+};
